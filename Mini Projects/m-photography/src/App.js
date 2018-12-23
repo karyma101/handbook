c456ba4b8a8edc5e88af79763b6c148a85821823
karyma101 <karyma101@hotmail.com>
2018-12-22 17:51:16 -0800

    set up routers

--- Mini Projects/m-photography/src/App.js
+++ Mini Projects/m-photography/src/App.js
@@ -1,27 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Home, About, Photography, Contact } from './components'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div class="left">
+          <div class="brand">
+            <img src="./img/logo.jpg" alt=""/>
+            <h1 class="brand_title">M. Photography</h1>
+          </div>
+
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/photography">Photography</Link>
+            <Link to="/contact">Contact</Link>
+          </nav>
+        </div>
+
+        <div class="right">
+          <Switch>
+            <Route
+              path="/" exact
+              component={<Home />}
+            ></Route>
+
+            <Route
+              path="/about"
+              component={<About />}
+            ></Route>
+
+            <Route
+              path="/photography"
+              component={<Photography />}
+            ></Route>
+
+            <Route
+              path="/contact" 
+              component={<Contact />}
+            ></Route>
+          </Switch>
+        </div>
       </div>
-    );
+    )
   }
 }
 
---
+++ Mini Projects/m-photography/src/components/index.js
@@ -0,0 +1,11 @@
+import Home from './Home'
+import About from './About'
+import Photography from './Photography'
+import Contact from './Contact'
+
+export {
+  Home,
+  About,
+  Photography,
+  Contact
+}
