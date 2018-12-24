e813ad54fccbb2d7e77aaeff567b7ae2aee68ffb
karyma101 <karyma101@hotmail.com>
2018-12-24 06:33:37 -0800

    carousel

--- Mini Projects/m-photography/src/components/Home.js
+++ Mini Projects/m-photography/src/components/Home.js
@@ -17,9 +17,39 @@ const Home = () => {
         </div>
       </header>
 
-      <section class="main">
-        <p>Hello World</p>
+      <section className="container py-5">
+        <div className="row">
+          <div className="col-12 col-md-6">
+            <h2 className="text-center">First</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iste eius nobis quam eum dignissimos, illum ipsam fugiat voluptates voluptas ea veniam, voluptatum consequatur dicta ratione nostrum modi distinctio magnam!</p>
+          </div>
+          
+          <div className="col-12 col-md-6">
+            <h2 className="text-center">Second</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae iste eius nobis quam eum dignissimos, illum ipsam fugiat voluptates voluptas ea veniam, voluptatum consequatur dicta ratione nostrum modi distinctio magnam!</p>
+          </div>
+        </div>
       </section>
+
+      {/* <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img className="d-block w-100" src="./img/social/facebook.png" alt="First slide"/>
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="./img/social/instagram.png" alt="Second slide"/>
+          </div>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="./img/social/linkedin.png" alt="Third slide"/>
+          </div>
+        </div>
+        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        </a>
+      </div> */}
     </div>
    )
 }
