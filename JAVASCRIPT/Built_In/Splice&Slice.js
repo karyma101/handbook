a124f611d5c507089025d3c19a2c3f492cc0208d
Kary Ma <karyma101@hotmail.com>
2019-07-08 16:41:01 -0700

    splice and slice example

---
+++ JAVASCRIPT/Built_In/Splice&Slice.js
@@ -0,0 +1,12 @@
+//splice
+var array=[1,2,3,4,5];
+console.log(array.splice(2));
+
+//slice
+var array2=[1,2,3,4,5]
+console.log(array2.slice(2));
+
+
+console.log("----after-----");
+console.log(array);
+console.log(array2);
