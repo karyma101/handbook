657152575b7258a9a69275cb572699723771556e
Kary Ma <karyma101@hotmail.com>
2019-08-26 18:30:27 -0700

    added react cheatsheet project

---
+++ JAVASCRIPT/Libraries/React/cheatsheet/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Classes from './components/Classes'
+import Functions from './components/Functions'
+
+const App = () => {
+  return (
+    <div>
+      <Classes />
+      <Functions />
+    </div>
+  )
+}
+
+export default App
---
+++ JAVASCRIPT/Libraries/React/cheatsheet/src/components/Functions.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Functions = () => {
+  return (
+    <Fragment>
+      <h1>Functional Component</h1>
+    </Fragment>
+  )
+}
+
+export default Functions
