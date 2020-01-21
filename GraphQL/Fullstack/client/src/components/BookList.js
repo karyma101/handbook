98beb580476aefdf44a44184b97da3544ace7f73
Kary Ma <karyma101@hotmail.com>
2020-01-21 14:27:08 -0800

    added apollo client

---
+++ GraphQL/Fullstack/client/src/components/BookList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { gql } from 'apollo-boost'
+import { graphql } from 'react-apollo'
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`
+
+class BookList extends Component {
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book Name</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default graphql(getBooksQuery)(BookList)
