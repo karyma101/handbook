ecd88135b234fad978ad14afce7368364bb8ae44
Kary Ma <karyma101@hotmail.com>
2020-01-22 12:33:15 -0800

    add book component

---
+++ GraphQL/Fullstack/client/src/components/AddBook.js
@@ -0,0 +1,40 @@
+import React,{ Component } from 'react'
+import { gql } from 'apollo-boost'
+import { graphql } from 'react-apollo'
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      id
+    }
+  }
+`
+
+class AddBook extends Component{
+  
+  render(){
+    return (
+      <form id="add-book">
+        <div className="field">
+          <label>Book name:</label>
+          <input type="text"/>
+        </div>
+
+        <div className="field">
+          <label>Genre:</label>
+          <input type="text"/>
+        </div>
+
+        <div className="field">
+          <label>Author:</label>
+          <select>
+            <option>Select Author</option>
+          </select>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default graphql(getAuthorsQuery)(AddBook)
