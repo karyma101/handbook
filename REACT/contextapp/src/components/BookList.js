3198ab073b4f48ef536b9d76d3df42736c667840
Kary Ma <karyma101@hotmail.com>
2020-01-24 15:36:08 -0800

    functional context component

--- REACT/contextapp/src/components/BookList.js
+++ REACT/contextapp/src/components/BookList.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext'
+import { BookContext } from '../contexts/BookContext';
 
 // class BookList extends Component {
 //   static contextType = ThemeContext
@@ -20,13 +21,14 @@ import { ThemeContext } from '../contexts/ThemeContext'
 
 const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext)
+  const { books } = useContext(BookContext)
   const theme = isLightTheme ? light : dark
   return ( 
     <div className="book-list" style={{background: theme.bg, color: theme.syntax}}>
       <ul>
-        <li style={{ background: theme.ui }}>the way of kings</li>
-        <li style={{ background: theme.ui }}>the name of the wind</li>
-        <li style={{ background: theme.ui }}>the final empire</li>
+        {books.map(book => {
+          return <li style={{background: theme.ui }}key={book.id}>{book.title}</li>
+        })}
       </ul>
     </div>
   );
