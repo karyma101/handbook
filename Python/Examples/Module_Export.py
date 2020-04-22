87fbb89b1171938f7b6000d1086e3dc843aba5a2
Kary Ma <karyma101@hotmail.com>
2020-04-21 19:42:13 -0700

    Python notes - Modules

---
+++ Python/Examples/Module_Export.py
@@ -0,0 +1,14 @@
+def add(first_num, second_num):
+    return first_num + second_num
+
+
+def subtract(first_num, second_num):
+    return first_num - second_num
+
+
+def multiply(first_num, second_num):
+    return first_num * second_num
+
+
+def divide(first_num, second_num):
+    return first_num / second_num
